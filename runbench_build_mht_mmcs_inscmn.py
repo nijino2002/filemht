7f17f7054e7fbdbac74b576d884ccf47000493f1
nijino2002 <dlu@xidian.edu.cn>
2022-05-29 23:05:14 +0800

    added runbench_build_mht_mmcs_inscmn.py

---
+++ runbench_build_mht_mmcs_inscmn.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+for i in range(0,12):
+	print("choice:"+str(i))
+	os.system("./prfm_eval_build_mht_mmcs 1 " + str(i))
+	time.sleep(1)	#sleep for 1s
+	print('**********************************************************************')
